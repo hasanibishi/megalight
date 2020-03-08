27a1aecfc17e9b5e4064580e1e4feebeeabeb4d8
unknown <hasan.ibishi@gmail.com>
2020-03-08 14:46:11 +0100

    ROutes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './_components/home/home.component';
+import { GalleryComponent } from './_components/gallery/gallery.component';
+import { CatalogueComponent } from './_components/catalogue/catalogue.component';
+import { ContactComponent } from './_components/contact/contact.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'home', component: HomeComponent
+},
+{
+  path: 'gallery', component: GalleryComponent
+},
+{
+  path: 'catalogue', component: CatalogueComponent
+},
+{
+  path: 'contact', component: ContactComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HomeComponent } from './_components/home/home.component';
+import { GalleryComponent } from './_components/gallery/gallery.component';
+import { CatalogueComponent } from './_components/catalogue/catalogue.component';
+import { ContactComponent } from './_components/contact/contact.component';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GalleryComponent,
+    CatalogueComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
