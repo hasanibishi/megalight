b69068596273d54f411cb07877bd5ee60c371bdd
HasanIbishi <hasan.ibishi@hotmail.com>
2020-05-25 18:44:41 +0200

    Lang

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,40 @@
 import { Component, HostListener } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { animate, style, transition, trigger, state } from "@angular/animations";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({
+        overflow: 'hidden',
+        height: '*',
+        width: '300px'
+      })),
+      state('out', style({
+        opacity: '0',
+        overflow: 'hidden',
+        height: '0px',
+        width: '0px'
+      })),
+      transition('in => out', animate('400ms ease-in-out')),
+      transition('out => in', animate('400ms ease-in-out'))
+    ])
+  ]
 })
 export class AppComponent {
+  
+  // VARIABLES
   showScroll: boolean;
   showScrollHeight = 300;
   hideScrollHeight = 10;
 
-  // VARIABLES
   DatetimeNow: Date = new Date();
   Year: number;
-  showMenu = false;
+  selectedLanguage: string;
+  MenuOpened = 'out';
 
   MenuList: any[] = [{
     ID: 1,
@@ -43,21 +63,23 @@ export class AppComponent {
     localStorage.setItem('language', 'al');
     const language: string = localStorage.getItem('language');
     this.translate.use(language);
+    this.selectedLanguage = language;
 
     this.Year = this.DatetimeNow.getFullYear();
   }
 
   // TOGGLE MENU
   toggleMenu() {
-    this.showMenu = !this.showMenu;
+    this.MenuOpened = this.MenuOpened === 'out' ? 'in' : 'out';;
   }
 
   // CHANGE LANGUAGE
   changeLanguage(language: string) {
+    this.selectedLanguage = language;
     localStorage.setItem('language', language);
     this.translate.use(language);
   }
-  
+
   @HostListener('window:scroll', [])
   onWindowScroll() {
     if ((window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop) > this.showScrollHeight) {
